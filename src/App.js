9b5970db5b5672e869368b119424edbcadc57c09
Donald Obed Aguirre Paez <donald@pop-os.localdomain>
2022-03-02 14:37:39 -0600

    Restructure folders and files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './style.css';
 
-import Title from './components/Title';
-import Card from './components/Card';
+import Title from './components/presentation/Title';
+import Card from './components/container/Card';
 
 export default function App() {
   return (
